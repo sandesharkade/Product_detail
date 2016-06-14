fc4597a678911250f839719dc93593c441562bc5
ajay04 <abchm02@gmail.com>
2016-06-14 09:46:27 +0530

    access validation

--- product/views.py
+++ product/views.py
@@ -1,11 +1,12 @@
 from .models import Products
 from django.contrib.auth.models import User
 from django.views.generic import (ListView, DetailView,
-                                  UpdateView,DeleteView,CreateView,View)
+                                  UpdateView, DeleteView, CreateView, View)
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
 from mysite import settings
+from django.shortcuts import render
 
 
 class Product_list(ListView):
@@ -25,9 +26,16 @@ class Product_detail(DetailView):
     '''
     Display the product detail
     '''
+
     model = Products
+
     template_name = "product/product_detalis.html"
     context_object_name = "product"
+
+    # def get(self, request, pk):
+        # return Products.objects.filter(user=self.request.user, id=pk)
+        # return render(request, 'product/product_detalis.html', {'product':
+        # product})
 
 
 class Product_edit(UpdateView):
@@ -35,6 +43,7 @@ class Product_edit(UpdateView):
     '''
     Display the product edit
     '''
+
     template_name = "product/product_edit.html"
     model = Products
     fields = ['pname', 'photo', 'description']
@@ -47,8 +56,8 @@ class Product_delete(DeleteView):
      product delete
     '''
 
-    model=Products
-    success_url=reverse_lazy("product:product_list")
+    model = Products
+    success_url = reverse_lazy("product:product_list")
 
 
 class Product_save(CreateView):
@@ -62,8 +71,11 @@ class Product_save(CreateView):
     success_url = reverse_lazy("product:product_list")
 
     def form_valid(self, form):
+        print(self.request.user)
+
         user = self.request.user
         form.instance.user = user
+
         return super(Product_save, self).form_valid(form)
 
 
