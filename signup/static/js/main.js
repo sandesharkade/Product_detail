b6f58aee8bc97848e7c14ca5339f8a7d5c59ee91
ajay04 <abchm02@gmail.com>
2016-06-24 11:40:42 +0530

    ajax

---
+++ signup/static/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+  // register ajax
+  $("#register").click(function(){
+    // Checking for blank fields.
+    if( $("#username").val() == '' || $("#password").val()==''){
+      $('input[id="username"],input[id="password"],input[id="confirm_password"]').css("border","2px solid red");
+      $("#error").html("Please fill all the fields!");
+    } else if($("#password").val().length < 5){
+      $("#error").html("Password length should be more than 4");
+    } else if( $("#confirm_password").val()!=$("#password").val()){
+      $("#error").html("Passwords don't match. Try again!")
+    } else {
+      $.ajax({
+        type: "POST",
+        url:"register",
+        data: $('#register_form').serialize(),
+        success: function(data)
+        {
+          window.location = '';
+        },
+        error: function(data){
+          $("#error").html("Username already exists!");
+        }
+      });
+    }
+  });
+
+  //login ajax
+  $("#login").click(function(){
+    if( $("#u_name").val() =='' || $("#login_password").val() ==''){
+      $('input[id="u_name"],input[id="login_password"]').css("border","2px solid red");
+      $("#error_login").html("Please fill all the fields!");
+    }  else {
+      $.ajax({
+        type: "POST",
+        url:"/",
+        data: $('#login_form').serialize(),
+        success: function(data)
+        {
+          window.location = 'product';
+        },
+        error: function(data){
+          $("#error_login").html("Please check username or password");
+        }
+      });
+    }
+  });
+});
--- signup/views.py
+++ signup/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .forms import SignupForm
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView, FormView
 
@@ -12,23 +12,15 @@ class Register_user(FormView):
     To add a user
     '''
 
-    def get(self, request):
-        user_form = SignupForm
-        if request.user.is_active:
-            return HttpResponseRedirect('product')
-        return render(request, 'signup/register.html',
-                      {'user_form': user_form})
-
     def post(self, request):
         form = SignupForm(data=request.POST)
         if form.is_valid():
             user = form.save()
             user.set_password(user.password)
             user.save()
-            return HttpResponseRedirect('/')
-
+            return HttpResponse(status=200)
         else:
-            return render(request, 'signup/register.html', {'user_form': form})
+            return HttpResponse(status=400)
 
 
 class Login_view(TemplateView):
@@ -36,19 +28,18 @@ class Login_view(TemplateView):
     '''
     Login user code
     '''
-    def get(self,request):
+
+    def get(self, request):
         if request.user.is_active:
             return HttpResponseRedirect('product')
-        return render(request,'signup/login.html')
-
+        return render(request, 'signup/login.html')
 
     def post(self, request):
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST['u_name']
+        password = request.POST['login_password']
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect('product')
+            return HttpResponse(status=200)
         else:
-            msg = "Wrong Username OR Wrong Password"
-            return render(request, 'signup/login.html', {'msg': msg})
+            return HttpResponse(status=400)
